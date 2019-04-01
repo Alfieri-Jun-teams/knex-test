164b54cb8773fcc148b904c49855012e2125500b
Abbet Wang <abbetwang@gmail.com>
2019-04-01 22:20:25 +0800

    add mysql

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('knex,hi')
+const knex = require('knex')({
+    dialect: 'mysql',
+    connection:{
+        host:'140.143.194.238',
+        user:'istar',
+        password:'iStar12345@',
+        database: 'test'
+    }
+})
+const data = knex.select('*').from('users')
+
+console.table(knex)
+console.log(data.id)
