da32880babf8ed0c8c80f962d7f8a8b476ab04b1
Abbet Wang <abbetwang@gmail.com>
2019-04-01 23:11:13 +0800

    surport typescript

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import Knex from 'knex'
+
+let knex : Knex =  Knex({
+    client: 'mysql',
+    connection:{
+        host:'140.143.194.238',
+        user:'istar',
+        password:'iStar12345@',
+        database: 'test'
+    }
+})
+
+async function getData(){
+
+   const data = await knex.select('*').from('users')
+   console.table(data)
+}
+getData()
